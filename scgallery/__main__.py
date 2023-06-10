4d82e29c2d3e0e4ce465d80dac5763c3de0ac29c
Peter Gadfort <gadfort@zeroasic.com>
2023-06-10 06:47:55 -0400

    matrix

--- scgallery/__main__.py
+++ scgallery/__main__.py
@@ -3,6 +3,7 @@
 import os
 import shutil
 import argparse
+import json
 
 import siliconcompiler
 from siliconcompiler.targets import asap7_demo, freepdk45_demo, skywater130_demo
@@ -15,6 +16,8 @@ def __design_has_runner(design):
 
 
 def __copy_chip_image(chip, gallery):
+    if not chip:
+        return
     jobname = chip.get('option', 'jobname')
     png = os.path.join(chip._getworkdir(),
                        f'{chip.design}.png')
@@ -22,12 +25,16 @@ def __copy_chip_image(chip, gallery):
         shutil.copy(png, os.path.join(gallery, f'{chip.design}_{jobname}.png'))
 
 
-def run_design(design, target):
+def __setup_design(design, target):
     try:
         chip = design.setup(target=target)
-    except FileNotFoundError:
+        return chip
+    except FileNotFoundError as e:
+        print(e)
         return None
 
+
+def run_design(chip):
     jobname = chip.get('option', 'target').split('.')[-1]
     chip.set('option', 'jobname', f"{chip.get('option', 'jobname')}_{jobname}")
 
@@ -70,20 +77,47 @@ if __name__ == "__main__":
                         help='Path to the gallery',
                         default=os.path.join('images', siliconcompiler.__version__))
 
+    parser.add_argument('-json',
+                        metavar='<path>',
+                        help='Record a github usable json matrix')
+
     args = parser.parse_args()
+
+    designs = all_designs()
+    if args.design:
+        designs = {args.design: designs[args.design]}
+
+    targets = all_targets
+    if args.target:
+        targets = {args.target: all_targets[args.target]}
+
+    if args.json:
+        matrix = {'include': []}
+        for design, design_obj in designs.items():
+            if not __design_has_runner(design_obj):
+                for target, target_obj in targets.items():
+                    chip = __setup_design(design_obj, target_obj)
+                    if chip:
+                        matrix['include'].append({
+                            'design': design,
+                            'target': target
+                        })
+            else:
+                matrix['include'].append({
+                    'design': design,
+                    'target': list(targets.keys())[0]
+                })
+        with open(args.json, 'w') as f:
+            if matrix['include']:
+                f.write(json.dumps(matrix))
+            else:
+                f.write('{}')
+        exit(0)
 
     gallery = os.path.abspath(args.gallery)
     os.makedirs(gallery, exist_ok=True)
 
-    designs = list(all_designs().values())
-    if args.design:
-        designs = [all_designs()[args.design]]
-
-    targets = list(all_targets.values())
-    if args.target:
-        targets = [all_targets[args.target]]
-
-    for design in designs:
+    for _, design in designs.items():
         print(f'Running {design.__name__}')
         if __design_has_runner(design):
             run = getattr(design, 'run')
@@ -93,6 +127,7 @@ if __name__ == "__main__":
             except Exception:
                 pass
         else:
-            for target in targets:
-                chip = run_design(design, target)
+            for _, target in targets.items():
+                chip = __setup_design(design, target)
+                chip = run_design(chip)
                 __copy_chip_image(chip, gallery)
