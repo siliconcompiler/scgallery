787a6c4154e4885b6ac3940d90d189cfd1f4f2b7
Peter Gadfort <gadfort@zeroasic.com>
2023-06-18 15:45:43 -0400

    black_parrot

--- scgallery/designs/__init__.py
+++ scgallery/designs/__init__.py
@@ -1,15 +1,17 @@
-from .sha3 import sha3
 from .aes import aes
-from .gcd import gcd
-from .ibex import ibex
+from .black_parrot import black_parrot
 from .dynamic_node import dynamic_node
-from .swerv import swerv
 from .ethmac import ethmac
-from .jpeg import jpeg
-from .uart import uart
+from .gcd import gcd
 from .heartbeat import heartbeat
+from .ibex import ibex
+from .jpeg import jpeg
+# from .microwatt import microwatt
 from .riscv32i import riscv32i
+from .sha3 import sha3
+from .swerv import swerv
 from .tiny_rocket import tiny_rocket
+from .uart import uart
 
 from .zerosoc import run_flat as zerosoc_flat
 from .zerosoc import run_hierarchy as zerosoc_hierarchy
@@ -18,17 +20,19 @@ from .zerosoc import run_hierarchy as zerosoc_hierarchy
 def all_designs():
     return {
         "aes": aes,
-        "gcd": gcd,
-        "sha3": sha3,
-        "ibex": ibex,
-        "ethmac": ethmac,
-        "swerv": swerv,
+        "black_parrot": black_parrot,
         "dynamic_node": dynamic_node,
-        "jpeg": jpeg,
-        "uart": uart,
-        "riscv32i": riscv32i,
-        "tiny_rocket": tiny_rocket,
+        "ethmac": ethmac,
+        "gcd": gcd,
         "heartbeat": heartbeat,
+        "ibex": ibex,
+        "jpeg": jpeg,
+        # "microwatt": microwatt,
+        "riscv32i": riscv32i,
+        "sha3": sha3,
+        "swerv": swerv,
+        "tiny_rocket": tiny_rocket,
+        "uart": uart,
         "zerosoc_flat": zerosoc_flat,
         "zerosoc_hierarchy": zerosoc_hierarchy
     }
---
+++ scgallery/designs/black_parrot/black_parrot.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import os
+
+from siliconcompiler import Chip
+from siliconcompiler.targets import freepdk45_demo
+from scgallery.libraries.freepdk45.fakeram45 import fakeram45
+
+
+def setup(target=freepdk45_demo,
+          use_cmd_file=False):
+    chip = Chip('black_parrot')
+
+    if use_cmd_file:
+        chip.create_cmdline(chip.design)
+
+    aes_root = os.path.dirname(__file__)
+    src_root = os.path.join(aes_root, 'src')
+    extra_root = os.path.join(aes_root, 'extra')
+    sdc_root = os.path.join(aes_root, 'constraints')
+
+    for src in ('pickled.v',):
+        chip.input(os.path.join(src_root, src))
+
+    if not chip.get('option', 'target'):
+        chip.load_target(target)
+
+    mainlib = chip.get('asic', 'logiclib')[0]
+    sdc = os.path.join(sdc_root, f'{mainlib}.sdc')
+    if not os.path.exists(sdc):
+        raise FileNotFoundError(f'Cannot find {sdc} constraints')
+
+    chip.input(os.path.join(sdc_root, f'{mainlib}.sdc'))
+
+    chip.set('option', 'define', 'SYNTHESIS')
+
+    if mainlib == 'nangate45':
+        chip.use(fakeram45)
+        chip.add('asic', 'macrolib', 'fakeram45_256x95')
+        chip.add('asic', 'macrolib', 'fakeram45_512x64')
+        chip.add('asic', 'macrolib', 'fakeram45_64x15')
+        chip.add('asic', 'macrolib', 'fakeram45_64x7')
+        chip.add('asic', 'macrolib', 'fakeram45_64x96')
+
+        chip.set('option', 'skipcheck', True)
+
+        chip.input(os.path.join(extra_root, f'{mainlib}.v'))
+
+        chip.set('constraint', 'outline', [(0, 0),
+                                           (1350, 1300)])
+        chip.set('constraint', 'corearea', [(10.07, 11.2),
+                                            (1340, 1290)])
+
+        chip.set('tool', 'yosys', 'task', 'syn_asic', 'var', 'strategy', 'AREA3')
+
+    return chip
+
+
+if __name__ == '__main__':
+    chip = setup(use_cmd_file=True)
+
+    chip.run()
+    chip.summary()
