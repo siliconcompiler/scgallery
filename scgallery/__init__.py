d1cc60253434c42156844dca13589bcfb7961ee4
Peter Gadfort <gadfort@zeroasic.com>
2023-07-02 02:13:48 -0400

    add version generation

--- scgallery/__init__.py
+++ scgallery/__init__.py
@@ -1,3 +1,9 @@
+try:
+    from scgallery._version import __version__
+except ImportError:
+    # This only exists in installations
+    __version__ = None
+
 from .gallery import Gallery
 from .gallery import main as _main
 
