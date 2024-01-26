86d0ca76bef9ac2610a311a92c190a66a4ecdb6c
Peter Gadfort <gadfort@zeroasic.com>
2024-01-26 13:40:21 -0500

    remove tile.sh

--- scripts/tile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-WORK_DIR=./montage_resized
-rm -rf "$WORK_DIR"
-mkdir -p "$WORK_DIR"
-find . -maxdepth 1 -name "*.png" -exec convert -resize 50% {} "$WORK_DIR"/{} \;
-montage -background '#000000' -geometry +1+1 "$WORK_DIR"/*.png montage.jpg
