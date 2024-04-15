fed4f6b8631eef1714c6ca72c20432c7ca1c7ecd
Peter Gadfort <peter.gadfort@gmail.com>
2024-04-15 10:25:52 -0400

    fix mock_alu resource generation script

--- scgallery/designs/mock_alu/generate_resources.sh
+++ scgallery/designs/mock_alu/generate_resources.sh
@@ -6,30 +6,26 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 pip3 install git+https://github.com/antonblanchard/vlsiffra@292ac2aa4747cb7e0b1cde656a8f6161e0e953f0
-pip3 install amaranth-yosys
+pip3 install amaranth-yosys==0.25.0.0.post75
 
-declare -A techs=( ["asap7"]="asap7sc7p5t_rvt" ["sky130hd"]="sky130hd" ["none"]="none")
-
-for tech in asap7 sky130hd none
-do
 for algo in hancarlson brentkung inferred koggestone ripple
 do
-echo "$tech / ${techs[$tech]} - $algo"
+echo "$algo"
 vlsi-multiplier --bits=64 \
     --algorithm=$algo \
-    --tech=$tech \
+    --tech=none \
     --register-post-ppa \
     --register-post-ppg \
-    --output=${src_path}/src/src/main/resources/mult_${algo}_${techs[$tech]}.v
+    --output=${src_path}/src/src/main/resources/mult_${algo}_none.v
 done
-echo "$tech / ${techs[$tech]} - brentkung (mac)"
+echo "brentkung (mac)"
 vlsi-multiplier --bits=64 \
     --multiply-add \
     --algorithm=brentkung \
-    --tech=$tech \
+    --tech=none \
     --register-post-ppa \
     --register-post-ppg \
-    --output=${src_path}/src/src/main/resources/mac_brentkung_${techs[$tech]}.v
+    --output=${src_path}/src/src/main/resources/mac_brentkung_none.v
 done
 
 pushd ${src_path}/src/src/main/resources/
