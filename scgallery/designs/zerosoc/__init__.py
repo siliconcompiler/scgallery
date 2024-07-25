6b6b12b58b656ab17db9e22090edbefa23bc5639
Peter <46405338+gadfort@users.noreply.github.com>
2024-07-25 10:30:21 -0400

    update zerosoc

--- scgallery/designs/zerosoc/__init__.py
+++ scgallery/designs/zerosoc/__init__.py
@@ -9,7 +9,7 @@ from siliconcompiler import package
 def register_zerosoc(chip):
     chip.register_source(name='zerosoc_data',
                          path='git+https://github.com/siliconcompiler/zerosoc',
-                         ref='c770a00f197b0f5b388adb8dce34b9ba22c54039')
+                         ref='9e07599f8dc80716e6a1c17006cec2f7339e468d')
 
 
 def init_zerosoc():
