91c8cd6f546b4f2c62a659cb321d7d33fbc663de
Peter <46405338+gadfort@users.noreply.github.com>
2024-10-28 14:08:22 -0400

    update zerosoc to new lamdalibio

--- scgallery/designs/zerosoc/__init__.py
+++ scgallery/designs/zerosoc/__init__.py
@@ -9,7 +9,7 @@ from siliconcompiler import package
 def register_zerosoc(chip):
     chip.register_source(name='zerosoc_data',
                          path='git+https://github.com/siliconcompiler/zerosoc',
-                         ref='1fc021cb2f6c066fea7a4b942f0dcd0bd9567a9c')
+                         ref='faff9d79359564df72a46f02e2e0bbe56fd9e1b2')
 
 
 def init_zerosoc():
