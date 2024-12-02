de2ce57beb5f451f2c32896c23ac87b0a8a17427
Peter Gadfort <peter.gadfort@gmail.com>
2024-12-02 16:15:51 -0700

    avoid removing buffers

--- scgallery/designs/tiny_rocket/tiny_rocket.py
+++ scgallery/designs/tiny_rocket/tiny_rocket.py
@@ -31,7 +31,8 @@ def setup_physical(chip):
     chip.set('option', 'define', 'SYNTHESIS')
 
     if chip.get('option', 'pdk') == 'ihp130':
-        chip.set('tool', 'openroad', 'task', 'macro_placement', 'var', 'rtlmp_enable', 'true')
+        chip.set('tool', 'openroad', 'task', 'init_floorplan', 'var',
+                 'remove_synth_buffers', False)
 
 
 if __name__ == '__main__':
