1cd6c9c001c5ec1efc25ecf164722eec20615f0c
Peter Gadfort <peter.gadfort@gmail.com>
2025-02-05 14:43:28 -0700

    make data consistant

--- scripts/generate_data_table.py
+++ scripts/generate_data_table.py
@@ -7,7 +7,7 @@ import tempfile
 import zipfile
 
 from PIL import Image
-from siliconcompiler import Schema
+from siliconcompiler import Chip, Schema
 
 
 METRICS = [
@@ -23,25 +23,35 @@ def __extract_zip(zip_file, workdir):
         f.extractall(path=workdir)
 
 
-def process_images(zip_file, workdir):
+def process_images(img_file, workdir):
     image_dir = os.path.join(workdir, 'images')
-    __extract_zip(zip_file, image_dir)
+    _, ext = os.path.splitext(img_file)
+    if ext == "":
+        image_dir = img_file
+    if ext == ".zip":
+        __extract_zip(img_file, image_dir)
 
     base_image_dir = image_dir
     base_image_dir = os.path.join(base_image_dir, os.listdir(base_image_dir)[0])
 
     images = {}
     for file in os.listdir(base_image_dir):
-        images[os.path.splitext(file)[0]] = {
-            "path": os.path.join(base_image_dir, file),
-            "relpath": os.path.relpath(os.path.join(base_image_dir, file), image_dir)
-        }
+        name, ext = os.path.splitext(file)
+        if ext == ".png":
+            images[name] = {
+                "path": os.path.join(base_image_dir, file),
+                "relpath": os.path.relpath(os.path.join(base_image_dir, file), image_dir)
+            }
     return images
 
 
-def process_manifests(zip_file, workdir):
+def process_manifests(manifest_file, workdir):
     manifest_dir = os.path.join(workdir, 'manifests')
-    __extract_zip(zip_file, manifest_dir)
+    _, ext = os.path.splitext(manifest_file)
+    if ext == "":
+        manifest_dir = manifest_file
+    if ext == ".zip":
+        __extract_zip(manifest_file, manifest_dir)
 
     data = []
 
@@ -54,17 +64,26 @@ def process_manifests(zip_file, workdir):
             variant_path = os.path.join(design_path, variant)
 
             manifest = os.path.join(variant_path, f"{design}.pkg.json")
+            if not os.path.exists(manifest):
+                manifest = None
 
             design_data = {
                 "design": design,
                 "variant": variant,
-                "manifest": os.path.relpath(manifest, manifest_dir),
+                "manifest": manifest,
                 "success": "yes",
-                "description": ""
+                "description": "",
+                "pdk": None,
+                "mainlib": None,
+                "image": None
             }
+            for step, index, metric in METRICS:
+                design_data[f"metric-{metric}"] = None
+            data.append(design_data)
 
-            if not os.path.exists(manifest):
+            if not manifest:
                 design_data["success"] = "no"
+                continue
 
             schema = Schema(manifest=manifest)
 
@@ -79,7 +98,6 @@ def process_manifests(zip_file, workdir):
             design_data["pdk"] = schema.get('option', 'pdk')
             design_data["mainlib"] = schema.get('asic', 'logiclib')[0]
 
-            data.append(design_data)
     return data
 
 
@@ -96,19 +114,24 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--images",
-        help="Path to image zip",
-        metavar="<file>",
+        help="Path to image",
+        metavar="<file/dir>",
         required=True)
 
     parser.add_argument(
         "--manifests",
-        help="Path to manifest zip",
-        metavar="<file>",
+        help="Path to manifest",
+        metavar="<file/dir>",
         required=True)
 
     parser.add_argument(
         "--merge",
         help="Merge with exisiting data",
+        action="store_true")
+
+    parser.add_argument(
+        "--save_manifest",
+        help="Preserve manufest data",
         action="store_true")
 
     args = parser.parse_args()
@@ -120,7 +143,8 @@ if __name__ == "__main__":
         for dat in data:
             image_name = f"{dat['design']}_{dat['variant']}"
             if image_name in images:
-                img_path = os.path.join(args.output, images[image_name]["relpath"])
+                img_path = os.path.join(args.output,
+                                        os.path.basename(images[image_name]["relpath"]))
                 img_path, _ = os.path.splitext(img_path)
                 img_path += ".webp"
                 os.makedirs(os.path.dirname(img_path), exist_ok=True)
@@ -130,8 +154,16 @@ if __name__ == "__main__":
                 img.save(img_path, "WEBP")
 
                 dat["image"] = os.path.relpath(img_path, args.output)
+
+            if dat["manifest"] and args.save_manifest:
+                chip = Chip("")
+                chip.schema = Schema(manifest=dat["manifest"])
+                manifest_path = os.path.join(args.output,
+                                             os.path.basename(dat["manifest"]) + ".gz")
+                chip.write_manifest(manifest_path)
+                dat["manifest"] = manifest_path
             else:
-                dat["image"] = None
+                dat["manifest"] = None
 
         if args.merge:
             with open(os.path.join(args.output, "summary.json")) as f:
