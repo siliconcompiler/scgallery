6b5c7f66de7319586ba5544df8e1b83f156d5818
Peter Gadfort <peter.gadfort@gmail.com>
2025-03-16 17:32:13 -0400

    fix lint

--- scgallery/designs/darkriscv/darkriscv.py
+++ scgallery/designs/darkriscv/darkriscv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """
-Developed in a magic night of 19 Aug, 2018 between 2am and 8am, the DarkRISCV softcore started as an proof of concept for the opensource RISC-V instruction set.
+Developed in a magic night of 19 Aug, 2018 between 2am and 8am, the DarkRISCV softcore started as
+an proof of concept for the opensource RISC-V instruction set.
 
-Although the code is small and crude when compared with other RISC-V implementations, the DarkRISCV has lots of impressive features:
+Although the code is small and crude when compared with other RISC-V implementations, the DarkRISCV
+has lots of impressive features:
 
 implements most of the RISC-V RV32E instruction set
 implements most of the RISC-V RV32I instruction set
@@ -11,10 +13,12 @@ works up to 250MHz in a ultrascale ku040 (400MHz w/ overclock!)
 up to 100MHz in a cheap spartan-6, fits in small spartan-3E such as XC3S100E!
 can sustain 1 clock per instruction most of time (typically 70% of time)
 flexible harvard architecture (easy to integrate a cache controller, bus bridges, etc)
-works fine in a real xilinx (spartan-3, spartan-6, spartan-7, artix-7, kintex-7 and kintex ultrascale)
+works fine in a real xilinx (spartan-3, spartan-6, spartan-7, artix-7, kintex-7 and kintex
+ultrascale)
 works fine with some real altera and lattice FPGAs
 works fine with gcc 9.0.0 and above for RISC-V (no patches required!)
-uses between 850-1500LUTs (core only with LUT6 technology, depending of enabled features and optimizations)
+uses between 850-1500LUTs (core only with LUT6 technology, depending of enabled features and
+optimizations)
 optional RV32E support (works better with LUT4 FPGAs)
 optional 16x16-bit MAC instruction (for digital signal processing)
 optional coarse-grained multi-threading (MT)
