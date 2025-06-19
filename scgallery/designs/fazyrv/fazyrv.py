2c766b95ed857b37bfc89317d8b9d4f95074e4e7
Peter Gadfort <peter.gadfort@gmail.com>
2025-06-19 17:01:19 -0400

    fix fazyrv for ihp

--- scgallery/designs/fazyrv/fazyrv.py
+++ scgallery/designs/fazyrv/fazyrv.py
@@ -66,7 +66,7 @@ def setup_physical(chip):
     if chip.get('option', 'pdk') == 'ihp130':
         chip.set('constraint', 'aspectratio', 0.25)
         chip.set('tool', 'openroad', 'task', 'macro_placement', 'var', 'macro_place_halo',
-                 [20, 40])
+                 [20, 35])
     if chip.get('option', 'pdk') == 'skywater130':
         chip.set('constraint', 'aspectratio', 0.80)
 
