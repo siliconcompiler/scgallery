c76bd5a235e52a8429140c3774d34f032bb3f9e7
Peter Gadfort <peter.gadfort@gmail.com>
2025-10-11 18:34:16 -0400

    fix and enable checklist

--- scgallery/checklists/asicflow_rules.py
+++ scgallery/checklists/asicflow_rules.py
@@ -1,85 +1,77 @@
-from siliconcompiler import Checklist
 import json
 import fnmatch
 
+from typing import Optional, List, Tuple, Union
 
-def setup(job=None, flow=None, mainlib=None, flow_nodes=None, rules_files=None, skip_rules=None):
-    '''
-    '''
+from siliconcompiler import Checklist
 
-    if not rules_files:
-        raise ValueError('no rules file provided')
 
-    if not isinstance(rules_files, (list, tuple)):
-        rules_files = [rules_files]
+class ASICChecklist(Checklist):
+    def __init__(self,
+                 job: str,
+                 flow: str,
+                 mainlib: str,
+                 flow_nodes: List[Tuple[str, str]],
+                 rules_files: Union[str, List[str], Tuple[str, ...]],
+                 skip_rules: Optional[List[str]] = None):
+        super().__init__()
+        self.set_name("asicflow_rules")
 
-    standard = 'asicflow_rules'
+        if not rules_files:
+            raise ValueError('no rules file provided')
 
-    if not job:
-        raise ValueError('job is required')
+        if not isinstance(rules_files, (list, tuple)):
+            rules_files = [rules_files]
 
-    if not flow:
-        raise ValueError('flow is required')
+        # read in all rules
+        rules = {}
+        for rules_file in rules_files:
+            with open(rules_file, 'r') as f:
+                rules.update(json.load(f))
 
-    if not mainlib:
-        raise ValueError('mainlib is required')
+        if mainlib not in rules:
+            raise ValueError(f'{mainlib} is missing from rules')
 
-    if flow_nodes is None:
-        raise ValueError('flow_nodes is required')
+        if flow not in rules[mainlib]:
+            raise ValueError(f'{flow} is missing from rules')
 
-    checklist = Checklist(standard)
+        rules = rules[mainlib][flow]["rules"]
 
-    # read in all rules
-    rules = {}
-    for rules_file in rules_files:
-        with open(rules_file, 'r') as f:
-            rules.update(json.load(f))
+        for name, info in rules.items():
+            criteria = set()
+            nodes = set()
 
-    if mainlib not in rules:
-        raise ValueError(f'{mainlib} is missing from rules')
+            if skip_rules:
+                skip = False
+                for skip_rule in skip_rules:
+                    if fnmatch.fnmatch(name, skip_rule):
+                        skip = True
+                if skip:
+                    continue
 
-    if flow not in rules[mainlib]:
-        raise ValueError(f'{flow} is missing from rules')
-
-    rules = rules[mainlib][flow]["rules"]
-
-    for name, info in rules.items():
-        criteria = set()
-        nodes = set()
-
-        if skip_rules:
-            skip = False
-            for skip_rule in skip_rules:
-                if fnmatch.fnmatch(name, skip_rule):
-                    skip = True
-            if skip:
-                continue
-
-        for node in info['nodes']:
-            if node['step'] == '*':
-                steps = [step for step, _ in flow_nodes]
-            else:
-                steps = [node['step']]
-
-            for step in steps:
-                if node['index'] == '*':
-                    indexes = [index for nstep, index in flow_nodes if step == nstep]
+            for node in info['nodes']:
+                if node['step'] == '*':
+                    steps = [step for step, _ in flow_nodes]
                 else:
-                    indexes = [node['index']]
+                    steps = [node['step']]
 
-                for index in indexes:
-                    nodes.add((job, step, index))
+                for step in steps:
+                    if node['index'] == '*':
+                        indexes = [index for nstep, index in flow_nodes if step == nstep]
+                    else:
+                        indexes = [node['index']]
 
-        for rule in info['criteria']:
-            if rule['value'] is None:
-                continue
+                    for index in indexes:
+                        nodes.add((job, step, index))
 
-            criteria.add(f"{rule['metric']}{rule['operator']}{rule['value']}")
+            for rule in info['criteria']:
+                if rule['value'] is None:
+                    continue
 
-        checklist.set('checklist', standard, name, 'criteria', criteria)
-        checklist.add('checklist', standard, name, 'task', nodes)
+                criteria.add(f"{rule['metric']}{rule['operator']}{rule['value']}")
 
-    return checklist
+            self.set(name, 'criteria', criteria)
+            self.set(name, 'task', nodes)
 
 
 ##################################################
--- tests/test_checklist.py
+++ tests/test_checklist.py
@@ -1,94 +1,92 @@
 import pytest
-# import os
-# import json
-# import scgallery
-# from scgallery.checklists import asicflow_rules
-# from siliconcompiler import Chip
-# from siliconcompiler.tools._common.asic import get_mainlib
+import json
+import scgallery
 
-pytest.skip("needs updating", allow_module_level=True)
+import os.path
 
-
-# @pytest.fixture
-# def mainlib_name():
-#     return 'testlib'
+from scgallery.checklists.asicflow_rules import ASICChecklist
+from siliconcompiler import ASIC
 
 
-# @pytest.fixture
-# def flow_name():
-#     return 'testflow'
+@pytest.fixture
+def mainlib_name():
+    return 'testlib'
 
 
-# @pytest.fixture
-# def job_name():
-#     return 'jobtest'
+@pytest.fixture
+def flow_name():
+    return 'testflow'
 
 
-# @pytest.fixture
-# def rules_file(flow_name, mainlib_name):
-#     root = os.path.dirname(scgallery.__file__)
-#     with open(os.path.join(root, 'checklists/asicflow_template.json')) as f:
-#         rules = json.load(f)
-
-#     return {
-#         mainlib_name: {
-#             flow_name: {
-#                 "rules": rules
-#             }
-#         }
-#     }
+@pytest.fixture
+def job_name():
+    return 'jobtest'
 
 
-# @pytest.fixture
-# def check_chip(mainlib_name, flow_name, job_name):
-#     chip = Chip('test')
+@pytest.fixture
+def rules_file(flow_name, mainlib_name):
+    root = os.path.dirname(scgallery.__file__)
+    with open(os.path.join(root, 'checklists/asicflow_template.json')) as f:
+        rules = json.load(f)
 
-#     chip.set('option', 'flow', flow_name)
-#     chip.set('option', 'jobname', job_name)
-#     chip.set('asic', 'logiclib', mainlib_name)
-
-#     return chip
-
-
-# def test_checklist(rules_file, check_chip):
-#     with open('testrules.json', 'w') as f:
-#         json.dump(rules_file, f)
-
-#     checklist = asicflow_rules(
-#         job=check_chip.get('option', 'jobname'),
-#         flow=check_chip.get('option', 'flow'),
-#         mainlib=get_mainlib(check_chip),
-#         flow_nodes=[],
-#         rules_files=['testrules.json'])
-
-#     assert len(checklist.getkeys('checklist', 'asicflow_rules')) == 15
+    return {
+        mainlib_name: {
+            flow_name: {
+                "rules": rules
+            }
+        }
+    }
 
 
-# def test_checklist_skiprules_all(rules_file, check_chip):
-#     with open('testrules.json', 'w') as f:
-#         json.dump(rules_file, f)
+@pytest.fixture
+def check_project(mainlib_name, flow_name, job_name):
+    proj = ASIC()
+    proj.option.set_flow(flow_name)
+    proj.option.set_jobname(job_name)
+    proj.set("asic", "asiclib", mainlib_name)
 
-#     checklist = asicflow_rules(
-#         job=check_chip.get('option', 'jobname'),
-#         flow=check_chip.get('option', 'flow'),
-#         mainlib=get_mainlib(check_chip),
-#         flow_nodes=[],
-#         rules_files=['testrules.json'],
-#         skip_rules=['*'])
-
-#     assert 'asicflow_rules' not in checklist.getkeys('checklist')
+    return proj
 
 
-# def test_checklist_skiprules_runtime(rules_file, check_chip):
-#     with open('testrules.json', 'w') as f:
-#         json.dump(rules_file, f)
+def test_checklist(rules_file, check_project, mainlib_name):
+    with open('testrules.json', 'w') as f:
+        json.dump(rules_file, f)
 
-#     checklist = asicflow_rules(
-#         job=check_chip.get('option', 'jobname'),
-#         flow=check_chip.get('option', 'flow'),
-#         mainlib=get_mainlib(check_chip),
-#         flow_nodes=[],
-#         rules_files=['testrules.json'],
-#         skip_rules=['runtime*'])
+    checklist = ASICChecklist(
+        job=check_project.get('option', 'jobname'),
+        flow=check_project.get('option', 'flow'),
+        mainlib=mainlib_name,
+        flow_nodes=[],
+        rules_files=['testrules.json'])
 
-#     assert len(checklist.getkeys('checklist', 'asicflow_rules')) == 7
+    assert len(checklist.getkeys()) == 15
+
+
+def test_checklist_skiprules_all(rules_file, check_project, mainlib_name):
+    with open('testrules.json', 'w') as f:
+        json.dump(rules_file, f)
+
+    checklist = ASICChecklist(
+        job=check_project.get('option', 'jobname'),
+        flow=check_project.get('option', 'flow'),
+        mainlib=mainlib_name,
+        flow_nodes=[],
+        rules_files=['testrules.json'],
+        skip_rules=['*'])
+
+    assert len(checklist.getkeys()) == 0
+
+
+def test_checklist_skiprules_runtime(rules_file, check_project, mainlib_name):
+    with open('testrules.json', 'w') as f:
+        json.dump(rules_file, f)
+
+    checklist = ASICChecklist(
+        job=check_project.get('option', 'jobname'),
+        flow=check_project.get('option', 'flow'),
+        mainlib=mainlib_name,
+        flow_nodes=[],
+        rules_files=['testrules.json'],
+        skip_rules=['runtime*'])
+
+    assert len(checklist.getkeys()) == 7
